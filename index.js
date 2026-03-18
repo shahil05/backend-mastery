ef6a038055ffd43442618ec087767725fdaef043
shahil05 <shahilsingh5801@gmail.com>
2026-03-18 11:20:27 +0530

    day  : added multiple routes and understood server basics

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send("this is my backend jounrey")
 });
+app.get('/hello', (req,res) => {
+    res.send('hello sahil')
+});
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
